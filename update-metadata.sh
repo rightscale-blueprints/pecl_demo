54376aa6189f6c76159812b86afeca0fd185d666
Chris Fordham <chris.fordham@rightscale.com>
2012-12-11 21:12:32 +1100

    Upgrade update-metadata.sh to remove .gitignore for collectd* cookbooks; add in re-cached files.

--- update-metadata.sh
+++ update-metadata.sh
@@ -3,8 +3,8 @@
 # generate metadata.json for all upstream opscode cookbooks (metadata.json should not exist in development repositories)
 knife cookbook metadata apache2 -o cookbooks/
 knife cookbook metadata build-essential -o cookbooks/
-knife cookbook metadata collectd -o cookbooks/
-knife cookbook metadata collectd_plugins -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/			; rm ./cookbooks/collectd/.gitignore 
+knife cookbook metadata collectd_plugins -o cookbooks/  ; rm ./cookbooks/collectd_plugins/.gitignore 
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata mysql -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
